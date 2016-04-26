3767bcaea10739a7603c0ada235956e0bec1b382
dschau <dustinschau@gmail.com>
2016-04-26 09:11:47 -0500

    chore: various tweaks

--- test/index.spec.js
+++ test/index.spec.js
@@ -9,7 +9,19 @@ describe('removeArrayItem', () => {
     'red',
     ['red', 'green', ['red', 'blue']]
   ];
-  it('exposes a funtion', () => {
+  it('exposes a function', () => {
     expect(removeArrayItem).to.to.be.a('function');
+  });
+
+  it('flattens and removes items', () => {
+    const arr = [
+      ['red'],
+      ['red', ['green', ['red', 'blue']]],
+      ['red', ['red', ['red']]]
+    ];
+
+    expect(removeArrayItem(arr, 'red')).to.deep.equal([
+      [['green', ['blue']]]
+    ]);
   });
 });
